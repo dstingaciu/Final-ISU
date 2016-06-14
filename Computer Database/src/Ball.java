9f4ce7a212c567554bc40f7b28e69abde79c685b
zap12341 <stingaciu.sebastian@gmail.com>
2016-06-14 09:53:51 -0400

    Fixed a lot of things TODO: -Store play-by-play of each game -Add buttons and a nice menu

--- Computer Database/src/Ball.java
+++ Computer Database/src/Ball.java
@@ -1,32 +1,15 @@
 
 public class Ball {
 	
-	int dx,dy,maxx,maxy,pax,pay,pbx,pby,direction;
+	int dx,dy,maxx,maxy,pax,pay,pbx,pby,direction,p1=0,p2=0;
 	DataGUI gui=new DataGUI();
-	
-	public void Ball(int dx,int dy,int maxx,int maxy,int pax,int pay,int pbx,int pby){
+	public Ball(int dx,int dy,int maxx,int maxy){
 		this.dx=dx;
 		this.dy=dy;
 		this.maxx=maxx;
 		this.maxy=maxy;
-		this.pax=pax;
-		this.pay=pay;
-		this.pbx=pbx;
-		this.pby=pby;
 	}
-	public void main(){
-		direction=(int)(Math.random()*4+1);
-		if(direction==1)
-			dR();
-		else if(direction==2)
-			dL();
-		else if(direction==3)
-			uR();
-		else if(direction==4)
-			uL();
-		
-		
-	}
+
 	
 	public void dR(){
 		dx++;
@@ -44,42 +27,114 @@ public class Ball {
 		dx--;
 		dy--;
 	}
+	public int returnDX(){
+		return dx;
+	}
+	public int returnDY(){
+		return dy;
+	}
 	
+	public void main() throws InterruptedException{
+		direction=(int)(Math.random()*4+1);
+		if(direction==1)
+			dR();
+		else if(direction==2)
+			dL();
+		else if(direction==3)
+			uR();
+		else if(direction==4)
+			uL();
+		gui.getDX(dx);
+		gui.getDY(dy);
+		int n=3;
+		do{
+			n=hitDetection();
+		}while(n>2);
+		if(n==2){
+			gui.setWinner1();
+		}else{
+			gui.setWinner2();
+		}
+		
+	}
 	//Send x and y coordinates back to gui
 	public int hitDetection() throws InterruptedException{
-		Thread.sleep(1000);
+		Thread.sleep(3);
+
 		if(dx<=0){
+			p2++;
+			gui.setP2(p2);
+			dx=maxx/2;
+			dy=maxy/2;
 			return 1;
 		}else if(dx>maxx){
+			p1++;
+			gui.setP1(p1);
+			dx=maxx/2;
+			dy=maxy/2;
 			return 2;
 		}
 		
-		if(direction==1&&dy>=maxy){
+		if((direction==1&&dy>=maxy-25)){
 			direction=3;
-			hitDetection();
-		}else if (direction==2&&dy>=maxy){
+			uR();
+			gui.getDX(dx);
+			gui.getDY(dy);
+		}else if (direction==2&&dy>=maxy-25){
+			dR();
 			direction=4;
-			hitDetection();
+			gui.getDX(dx);
+			gui.getDY(dy);
 		}else if(direction==3&&dy<=0){
 			direction=1;
-			hitDetection();
+			dR();
+			gui.getDX(dx);
+			gui.getDY(dy);
+			
 		}else if(direction==4&&dy<=0){
 			direction=2;
-			hitDetection();
+			dL();
+			gui.getDX(dx);
+			gui.getDY(dy);
+			
+		}else if(direction==1&&(dx==gui.returnAX()-30&&dy>gui.returnAY()&&dy<gui.returnAY()+150)){
+			direction=2;
+			dL();
+			gui.getDX(dx);
+			gui.getDY(dy);
+		}else if(direction==2&&(dx==gui.returnX()+30&&dy>gui.returnY()&&dy<gui.returnY()+150)){
+			direction=1;
+			dR();
+			gui.getDX(dx);
+			gui.getDY(dy);
+		}else if(direction==3&&(dx==gui.returnAX()-30&&dy>gui.returnAY()&&dy<gui.returnAY()+150)){
+			direction=4;
+			uL();
+			gui.getDX(dx);
+			gui.getDY(dy);
+		}else if (direction==4&&(dx==gui.returnX()+30&&dy>gui.returnY()&&dy<gui.returnY()+150)){
+			direction=3;
+			uR();
+			gui.getDX(dx);
+			gui.getDY(dy);
 		}
 		
 		if(direction==1){
 			dR();
-			hitDetection();
+			gui.getDX(dx);
+			gui.getDY(dy);
 		}else if(direction==2){
 			dL();
-			hitDetection();
+			gui.getDX(dx);
+			gui.getDY(dy);
 		}else if(direction==3){
 			uR();
-			hitDetection();
+			gui.getDX(dx);
+			gui.getDY(dy);
 		}else if(direction==4){
 			uL();
-			hitDetection();
+			gui.getDX(dx);
+			gui.getDY(dy);
 		}
 		return 3;
 	}
--- Computer Database/src/Keys.java
+++ Computer Database/src/Keys.java
@@ -11,7 +11,6 @@ public class Keys implements KeyListener {
 		}else if (e.getKeyCode()== KeyEvent.VK_DOWN&&gui.returnAY()+150<=1024){
 			y+=20;
 		}
-		System.out.println(y);
 		gui.getAXY(y);
 		if(e.getKeyCode()==KeyEvent.VK_ESCAPE){
 			System.exit(0);
